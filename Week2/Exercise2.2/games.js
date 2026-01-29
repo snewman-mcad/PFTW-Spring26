71312aecf84441f5719c9e3ee62032877f6de1f9
snewman-mcad <snewman@mcad.edu>
2026-01-29 16:39:35 -0500

    Created array of objects (favorite games)

---
+++ Week2/Exercise2.2/games.js
@@ -0,0 +1,21 @@
+let favGames = [
+    {
+        title: "Clue",
+        type: "logic puzzle",
+        numberOfPlayers: "3-6",
+        rating: 5,
+        shortDescription: "The object of the game is to figure out who murdered the victim, where the crime took place, and which weapon was used.",
+    }, {
+        title: "Stakd",
+        type: "strategy",
+        numberOfPlayers: "1-6",
+        rating: 4,
+        shortDescription: "A strategic game where you stack pieces, block other players from using their pieces, and be the last to lay a piece so that you win."
+    }, {
+        title: "Cubed",
+        type: "strategy",
+        numberOfPlayers: "1-11",
+        rating: 5,
+        shortDescription: "This is a version of the classic Dominos game but with 3D hexagons. You have to match color and height of the pieces to play a legal piece."
+    }
+]
