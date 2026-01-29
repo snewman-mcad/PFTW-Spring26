66a0f4414b3cbcf94329da10764be86347cfd40a
snewman-mcad <snewman@mcad.edu>
2026-01-29 12:37:30 -0500

    Added user prompt

--- Week2/Exercise2.1/movieArray.js
+++ Week2/Exercise2.1/movieArray.js
@@ -1,3 +1,8 @@
 let myMovies = ["Ready Player One", "Knives Out", "Free Guy", "Boss Level", "Death on the Nile"];
 
 console.log(myMovies);
+
+let newMovie = window.prompt("What is your favorite movie?");
+myMovies.push(newMovie);
+
+console.log(myMovies);
