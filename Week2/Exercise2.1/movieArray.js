72c2d6be899af62dd24b788b524a17b5517dc043
snewman-mcad <snewman@mcad.edu>
2026-01-29 14:06:20 -0500

    Changed array to a string and added comments

--- Week2/Exercise2.1/movieArray.js
+++ Week2/Exercise2.1/movieArray.js
@@ -1,8 +1,14 @@
 let myMovies = ["Ready Player One", "Knives Out", "Free Guy", "Boss Level", "Death on the Nile"];
+//checking the myMovies array
+console.log("These are my favorite movies:", myMovies);
 
-console.log(myMovies);
-
+//prompt user for their favorite movie
 let newMovie = window.prompt("What is your favorite movie?");
+//add user input to the myMovies array
 myMovies.push(newMovie);
 
-console.log(myMovies);
+//checking the addition of the the new movie to the array
+console.log("Updated list of movies:", myMovies);
+
+//changing the array to a string
+console.log("Updated list of movies: ", myMovies.toString());
