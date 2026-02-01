6a637a227c5f1993d0fb510bdb44a553daec1176
snewman-mcad <snewman@mcad.edu>
2026-02-01 15:31:53 -0500

    added alert for correct answer

--- Week2/Assignment2/questions.js
+++ Week2/Assignment2/questions.js
@@ -3,26 +3,29 @@ the questions are book titles
 and their authors are the answers*/
 const questionsAndAnswers = [
     {
-        question: "What author duo wrote Warping Minds & Other Misdemeanors",
+        question: "What author duo wrote Warping Minds & Other Misdemeanors?",
         answer: "Annette Marie and Rob Jacobsen",
     }, {
-        question: "Who wrote Fourth Wing",
+        question: "Who wrote Fourth Wing?",
         answer: "Rebecca Yarros",
     }, {
-        question: "Who wrote A Study in Scarlet",
+        question: "Who wrote A Study in Scarlet?",
         answer: "Arthur Conan Doyle",
     }, {
-        question: "Who wrote Death on the Nile",
+        question: "Who wrote Death on the Nile?",
         answer: "Agatha Christie",
     }, {
-        question: "Who wrote Ready Player One",
+        question: "Who wrote Ready Player One?",
         answer: "Ernest Cline",
     }, {
-        question: "Who wrote Divergent",
+        question: "Who wrote Divergent?",
         answer: "Veronica Roth",
     }, {
-        question: "Who wrote In the Courts of the Sun",
+        question: "Who wrote In the Courts of the Sun?",
         answer: "Brian D'Amato",
+    }, {
+        question: "Who wrote Midlife Bounty Hunter?",
+        answer: "Shannon Mayer",
     }
 ];
 
@@ -30,8 +33,12 @@ const questionsAndAnswers = [
 //added in Math.floor to round down to give whole numbers
 const randomNumber = Math.floor(Math.random() * questionsAndAnswers.length);
 
-//generates a random question, from the above array, for the user to answer
+//prompts user to answer a random question, from the array questionsAndAnswers
 let userAnswer = window.prompt(questionsAndAnswers[randomNumber].question);
 
 //alerts the user that they answered and what their answer was
 window.alert("You answered: " + userAnswer);
+
+/*alerts user to correct answer
+using the random number generated above (randomNumber) for the correct index*/
+window.alert("The correct answer was: " + questionsAndAnswers[randomNumber].answer);
