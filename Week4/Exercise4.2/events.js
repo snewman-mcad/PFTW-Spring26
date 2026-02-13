cd20b15c37fb6b328373e9233c4b8481c4c0e8d9
snewman-mcad <snewman@mcad.edu>
2026-02-12 20:22:01 -0500

    Square changes to random colors when clicked
    also added some comments

--- Week4/Exercise4.2/events.js
+++ Week4/Exercise4.2/events.js
@@ -4,19 +4,30 @@ let rectY;
 const rectHeight = 75;
 let clickCount = 0;
 let speed;
+let rectColor = 'turquoise';
+//starting color
+let randomColor = ['turquoise', 'lime', 'purple', 'gold', 'magenta', 'tomato'];
+//array of colors to change the square fill
 
 function setup () {
     createCanvas(500, 500);
-    rectY = random(height - rectWidth);
+    rectY = random(height - rectHeight);
+    //start point is from the square's height and the canvas
     speed = random(1, 5);
+    //number from 1 to 5 generated for the speed
 }
 
 function draw () {
     background(0);
     drawShape();
+    textSize(24);
+    fill('white');
+    text('Click the square as many times as you can!', 20, 450);
     rectX+=speed;
+    //square moves across the screen based on the speed increment
     if(rectX > width) {
         noLoop();
+        fill('white');
         text('Your score was ' + clickCount, 100, 300);
     }
 }
@@ -26,9 +37,15 @@ function mousePressed() {
         clickCount++;
         console.log('hit', clickCount);
     }
+    if(rectColor) {
+        let newColor = random(randomColor);
+        //generates a new color from the randomColor array
+        rectColor = newColor;
+        //assigns the new color to the square
+    }
 }
 
 function drawShape () {
-    fill('turquoise');
+    fill(rectColor);
     rect(rectX, rectY, rectWidth, rectHeight);
 }
