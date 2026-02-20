797545232aaa32ba3ea05df1da8fad25f7ebb93d
snewman-mcad <snewman@mcad.edu>
2026-02-19 20:46:30 -0500

    multiple bubbles generated in for loop

--- Week5/Exercise5.2/classes.js
+++ Week5/Exercise5.2/classes.js
@@ -1,9 +1,14 @@
-let bubble;
+let bubble1;
+let bubble2;
+let bubble3;
+let bubble4;
+let bubble5;
+let manyBubbles = [bubble1, bubble2, bubble3, bubble4, bubble5];
 
 function setup () {
     createCanvas(500, 500);
     background(0);
-    bubble = new Bubble(100, 200, 100);
+    //bubble = new Bubble(100, 200, 100);
 }
 
 class Bubble {
@@ -26,6 +31,13 @@ class Bubble {
 
 function draw () {
     background(0);
-    bubble.move();
-    bubble.show();
+    frameRate(3)
+    for(i = 0; i < manyBubbles.length; i++) {
+        let randomX = random(50, 350);
+        let randomY = random(70, 400);
+        let randomDiameter = random(50, 100);
+        manyBubbles[i] = new Bubble(randomX, randomY, randomDiameter);
+        manyBubbles[i].move();
+        manyBubbles[i].show();
+    }
 }
