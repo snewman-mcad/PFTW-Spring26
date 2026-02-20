365499eb7015b8138982283d54d43d0dcdbb8370
snewman-mcad <snewman@mcad.edu>
2026-02-19 22:02:56 -0500

    bubbles generated from objects in an array

--- Week5/Exercise5.2/classes.js
+++ Week5/Exercise5.2/classes.js
@@ -1,43 +1,64 @@
-let bubble1;
-let bubble2;
-let bubble3;
-let bubble4;
-let bubble5;
-let manyBubbles = [bubble1, bubble2, bubble3, bubble4, bubble5];
+//array of objects to create individual circles
+let manyBubbles = [
+    {
+        x: 50,
+        y: 100,
+        diameter: 100
+    }, {
+        x: 100,
+        y: 50,
+        diameter: 70
+    }, {
+        x: 200,
+        y: 300,
+        diameter: 70
+    }, {
+        x: 300,
+        y: 150,
+        diameter: 50
+    }, {
+        x: 300,
+        y: 200,
+        diameter: 125
+    }
+]
 
 function setup () {
     createCanvas(500, 500);
     background(0);
-    //bubble = new Bubble(100, 200, 100);
 }
 
 class Bubble {
     constructor (x, y, diameter) {
+        //properties for creating a circle
         this.x = x;
         this.y = y;
         this.diameter = diameter;
     }
     move () {
+        //changes the x and y values to move the circle
         this.x = this.x + random(-8, 8);
         this.y = this.y + random(-8, 8);
     }
     show () {
         stroke('turquoise');
         strokeWeight(2);
-        noFill();
+        //transparent blue color
+        fill(50, 60, 170, 70);
+        //draws a circle with the x, y, and diameter that are passed to it
         circle(this.x, this.y, this.diameter);
     }
 }
 
 function draw () {
     background(0);
-    frameRate(3)
+    frameRate(8)
     for(i = 0; i < manyBubbles.length; i++) {
-        let randomX = random(50, 350);
-        let randomY = random(70, 400);
-        let randomDiameter = random(50, 100);
-        manyBubbles[i] = new Bubble(randomX, randomY, randomDiameter);
+        //creates a new circle/bubble from the properties passed to it from the objects in the array
+        manyBubbles[i] = new Bubble(manyBubbles[i].x, manyBubbles[i].y, manyBubbles[i].diameter);
+        //calls move for each bubble/circle
         manyBubbles[i].move();
+        //calls show for each circle/bubble
         manyBubbles[i].show();
     }
 }
