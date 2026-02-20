f78f2e7bbaa1843b682d4b186393dc61cc572aab
snewman-mcad <snewman@mcad.edu>
2026-02-20 16:36:24 -0500

    Images to card fronts added; two card fronts per image

--- Week5/Assignment5/synthesis.js
+++ Week5/Assignment5/synthesis.js
@@ -8,14 +8,45 @@ const gameState = {
 
 }
 
+let cardFrontArray = [];
+function preload () {
+    cardFrontArray = [
+        loadImage('img/Articuno-150x200.png'),
+        loadImage('img/Butterfree-150x200.png'),
+        loadImage('img/Cyndaquil-150x200.png'),
+        loadImage('img/Mew-150x200.png'),
+        loadImage('img/Pikachu-150x200.png'),
+        loadImage('img/Porygon-150x200.png'),
+        loadImage('img/Totodile-150x200.png'),
+        loadImage('img/Umbreon-150x200.png'),
+        loadImage('img/Vaporeon-150x200.png'),
+    ]
+}
+
 function setup () {
     createCanvas(1150, 900);
     background('aliceblue');
+    let selectedFronts = [];
+    //looping through 9 times because that is the number of card pairs
+    for (let z = 0; z < 9; z++) {
+        //creating variable for random index number from the length of the array
+        const randomIndex = floor(random(cardFrontArray.length));
+        //choose random front from the array using random number that was generated
+        const front = cardFrontArray[randomIndex];
+        //adding the random front twice because two copies are needed
+        selectedFronts.push(front);
+        selectedFronts.push(front);
+        //remove the used card front so that it doesn't get randomly selected again
+        cardFrontArray.splice(randomIndex, 1);
+    }
     //making 3 rows of cards
     for (let j = 0; j < 3; j++) {
         //making 6 columns of cards
         for (let i = 0; i < 6; i++) {
-            cards.push(new Card(startingX, startingY));
+            //removes and returns last item in the array
+            const frontImage = selectedFronts.pop();
+            //adds returned image to the cards in the array
+            cards.push(new Card(startingX, startingY, frontImage));
             startingX += 180;
         }
         //reseting the x and y for the start of each row
@@ -27,19 +58,21 @@ function setup () {
 
 function mousePressed () {
     for (let k = 0; k < cards.length; k++) {
+        //checking if the cards in the array were clicked
         if (cards[k].didHit(mouseX, mouseY)) {
-            console.log('flipped');
+            console.log('flipped', cards[k]);
         }
     }
 }
 
 class Card {
-    constructor (x, y) {
+    constructor (x, y, cardFrontImg) {
         this.x = x;
         this.y = y;
         this.width = 150;
         this.height = 200;
         this.face = DOWN;
+        this.cardFrontImg = cardFrontImg
         this.show();
     }
     show () {
@@ -61,7 +94,8 @@ class Card {
             fill('white');
             strokeWeight(4);
             stroke('midnightblue');
-            rect(this.x, this.y, this.width, this.height, 10)
+            rect(this.x, this.y, this.width, this.height, 10);
+            image(this.cardFrontImg, this.x, this.y);
         }
     }
 
