e266807888a1315ac08f15f556dba75dd1365103
snewman-mcad <snewman@mcad.edu>
2026-03-01 20:38:09 -0500

    Added a comment and some css
    Tried to loop through the array but couldn't figure out how to do the ternary with it.

--- Week6/Assignment6/src/App.jsx
+++ Week6/Assignment6/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     "Llamas were domesticated from wild guanacos, also classified as Lama glama.",
     "Llamas are herbivores. Like cows, they also regurgitate their food and chew on their cud.",
     "Llamas weigh around 280 to 450 pounds and can carry about a quarter of their body weight."
-  ]
+  ];
 
   const packAnimal = true;
 
@@ -19,11 +19,11 @@ function App() {
         <li>{llamaFacts[0]}</li>
         <li>{llamaFacts[1]}</li>
         <li>{llamaFacts[2]}</li>
-        {/*ternary for whether a llama is a pack animal*/}
+        {/*ternary for whether a llama is a pack animal, will show the last fact in the array if true*/}
         <li>{packAnimal ? llamaFacts[4] : llamaFacts[3]}</li>
       </ul>
     </div>
-  )
+  );
 }
 
 export default App
