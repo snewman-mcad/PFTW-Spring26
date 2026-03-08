7cf67ac0598b43a4bf0767daa4a5ab36d7caa20f
snewman-mcad <snewman@mcad.edu>
2026-03-08 19:50:06 -0400

    Added css, prices to skeins, and conditional css for prices.

--- Week7/Assignment7/src/App.jsx
+++ Week7/Assignment7/src/App.jsx
@@ -10,42 +10,48 @@ function App() {
       yardage: 550,
       fiber: "50% Superwash Merino Wool and 50% Mulberry Silk",
       image: './june-alexandrite.jpg',
-      alt: "Skein of teal and violet yarn that has a shiny quality to it."
+      alt: "Skein of teal and violet yarn that has a shiny quality to it.",
+      price: 30
     },{
       name: "Naughty List",
       weight: "Worsted (4)",
       yardage: 220,
       fiber: "50% Superwash Merino Wool and 50% Mulberry Silk",
       image: './naughty-list.jpg',
-      alt: "Skein of dark purple, violet, and lilac yarn that has a shiny quality to it."
+      alt: "Skein of dark purple, violet, and lilac yarn that has a shiny quality to it.",
+      price: 30
     },{
       name: "Dragon Lore",
       weight: "Fingering (1)",
       yardage: 500,
       fiber: "50% Camel Silk and 50% Mulberry Silk",
       image: './dragon-lore.jpg',
-      alt: "Multi color skein of yarn. Colors included are teal, gold, white, and mulberry."
+      alt: "Multi color skein of yarn. Colors included are teal, gold, white, and mulberry.",
+      price: 37
     },{
       name: "Belladonna Kiss",
       weight: "Fingering (1)",
       yardage: 400,
       fiber: "100% Superwash Merino Wool",
       image: './belladonna-kiss.jpg',
-      alt: "A mostly dark purple skein of yarn with some splashes of white and raspberry."
+      alt: "A mostly dark purple skein of yarn with some splashes of white and raspberry.",
+      price: 26
     },{
       name: "October Opal",
       weight: "DK (3)",
       yardage: 250,
       fiber: "70% Superwash Merino Wool, 20% Nylon, and 10% Silk",
       image: './october-opal.jpg',
-      alt: "Skein of mostly white and turquoise yarn with some splashes of mint and pink."
+      alt: "Skein of mostly white and turquoise yarn with some splashes of mint and pink.",
+      price: 25
     },{
       name: "Blackcurrant Beauty",
       weight: "Fingering (1)",
       yardage: 400,
       fiber: "100% Superwash Merino Wool",
       image: './blackcurrant-beauty.jpg',
-      alt: "Skein of dark teal and purple yarn."
+      alt: "Skein of dark teal and purple yarn.",
+      price: 26
     }
   ];
 
@@ -63,9 +69,11 @@ function App() {
           alt={yarn.alt}
           weight={yarn.weight}
           fiber={yarn.fiber}
-          yardage={yarn.yardage}/>
+          yardage={yarn.yardage}
+          price={yarn.price}/>
         )
       })}
+      <p style={{fontSize: '1.2rem'}}>Please note: If a yarn/skein is expensive, <span className={"expensive"}>above $35</span>, the price will be marked <span className={"expensive"}>green and italic</span></p>
     </div>
   )
 }
--- Week7/Assignment7/src/components/Skein.jsx
+++ Week7/Assignment7/src/components/Skein.jsx
@@ -1,15 +1,16 @@
 import "./Skein.css";
 
-function Skein({name, image, alt, weight, yardage, fiber, odd}) {
+function Skein({name, image, alt, weight, yardage, fiber, price, odd}) {
     return(  
-        <div className={odd && "odd"}>
+        <div className={odd ? "odd" : "even"}>
             <div className={"grid"} key={name}>
                 <img src={image} alt={alt} />
-                <div className={odd && "odd-inner"}>
+                <div className={odd ? "odd-inner" : "even-inner"}>
                     <h2>{name}</h2>
                     <p>{"Yarn weight: " + weight}</p>
                     <p>{yardage + " yards per skein"}</p>
                     <p>{"Fiber content: " + fiber}</p>
+                    <p className={price > 35 ? "expensive" : ""}>{"$" + price + " per skein"}</p>
                 </div>
             </div>
         </div>
