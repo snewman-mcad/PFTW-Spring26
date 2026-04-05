37850c1d449877d69b562059d6e89be55f4e0e1a
snewman-mcad <snewman@mcad.edu>
2026-04-04 20:49:33 -0400

    Using params to return object from the key id

--- Week10/Assignment10/src/views/SkeinDetail.jsx
+++ Week10/Assignment10/src/views/SkeinDetail.jsx
@@ -1,5 +1,24 @@
+import { useParams } from 'react-router-dom';
+import yarnData from '../assets/yarnData.json';
+
 export function SkeinDetail() {
+    const {id} = useParams();
+    console.log("params: ", id);
+    console.log("yarn data: ", yarnData);
+    const selectedSkein = yarnData.find((yarn) => {
+        // using +symbol/operator to convert the string of id to a number, yarn.id is a number 
+        return yarn.id === +id
+    })
+    console.log("selected yarn: ", selectedSkein);
+
     return (
-        <h1>Skein Details</h1>
+        <div>
+            {selectedSkein !== undefined ? (
+            <div>
+                <h1>Skein Details</h1>
+                <h2>{selectedSkein.name}</h2>
+            </div>)
+            : (<p>The skein in not found.</p>)}
+        </div>
     )
 }
