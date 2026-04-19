6f4039a8633885b992a4760fd18f220b36054b14
snewman-mcad <snewman@mcad.edu>
2026-04-19 15:18:54 -0400

    volume for each sound is controlled by individual sliders

--- Week12/Assignment12/sound.js
+++ Week12/Assignment12/sound.js
@@ -1,49 +1,95 @@
 let myButtons = [];
 let allSliders;
+let slider0;
+let slider1;
+let slider2;
+let slider3;
+let slider4;
+let slider5;
+let slider6;
+let slider7;
 
 function preload() {
     soundFormats("wav", "m4a");
     //loading in some random instrumental sounds from freesound.org
-    let sound1 = loadSound("./Sounds/thirsk__128-cello-loop-2.wav");
-    let sound2 = loadSound("./Sounds/deleted_user_5205523__junk-drum-kit-loop-3.wav");
-    let sound3 = loadSound("./Sounds/chrisbutler99__tropical-loop-1.wav");
-    let sound4 = loadSound("./Sounds/violinsimma__violin-carnatic-phrase-am.wav");
-    let sound5 = loadSound("./Sounds/animastudios__steel-drum-105bpm-gm.wav");
-    let sound6 = loadSound("./Sounds/thesoundlibrary__drum-loop-2.wav");
-    let sound7 = loadSound("./Sounds/overfieldsofwheat__uplifting-acoustic-guitar.wav");
-    let sound8 = loadSound("./Sounds/eee3333e__guitar-and-piano-loop.m4a");
+    let sound0 = loadSound("./Sounds/thirsk__128-cello-loop-2.wav");
+    let sound1 = loadSound("./Sounds/deleted_user_5205523__junk-drum-kit-loop-3.wav");
+    let sound2 = loadSound("./Sounds/chrisbutler99__tropical-loop-1.wav");
+    let sound3 = loadSound("./Sounds/violinsimma__violin-carnatic-phrase-am.wav");
+    let sound4 = loadSound("./Sounds/animastudios__steel-drum-105bpm-gm.wav");
+    let sound5 = loadSound("./Sounds/thesoundlibrary__drum-loop-2.wav");
+    let sound6 = loadSound("./Sounds/overfieldsofwheat__uplifting-acoustic-guitar.wav");
+    let sound7 = loadSound("./Sounds/eee3333e__guitar-and-piano-loop.m4a");
 
     myButtons = [
-        { x: 70, y: 70, w: 100, h: 70, r: 15, sound: sound1, name: "cello"},
-        { x: 220, y: 70, w: 100, h: 70, r: 15, sound: sound2, name: "drum"},
-        { x: 370, y: 70, w: 100, h: 70, r: 15, sound: sound3, name: "trop"},
-        { x: 520, y: 70, w: 100, h: 70, r: 15, sound: sound4, name: "violin"},
-        { x: 70, y: 190, w: 100, h: 70, r: 15, sound: sound5, name: "steel"},
-        { x: 220, y: 190, w: 100, h: 70, r: 15, sound: sound6, name: "drum"},
-        { x: 370, y: 190, w: 100, h: 70, r: 15, sound: sound7, name: "guitar"},
-        { x: 520, y: 190, w: 100, h: 70, r: 15, sound: sound8, name: "piano"}
+        { x: 70, y: 70, w: 100, h: 70, r: 15, sound: sound0, name: "cello"},
+        { x: 220, y: 70, w: 100, h: 70, r: 15, sound: sound1, name: "drum"},
+        { x: 370, y: 70, w: 100, h: 70, r: 15, sound: sound2, name: "trop"},
+        { x: 520, y: 70, w: 100, h: 70, r: 15, sound: sound3, name: "violin"},
+        { x: 70, y: 230, w: 100, h: 70, r: 15, sound: sound4, name: "steel"},
+        { x: 220, y: 230, w: 100, h: 70, r: 15, sound: sound5, name: "drum"},
+        { x: 370, y: 230, w: 100, h: 70, r: 15, sound: sound6, name: "guitar"},
+        { x: 520, y: 230, w: 100, h: 70, r: 15, sound: sound7, name: "piano"}
     ];
 
     allSliders = [
-        { x: 120, y: 170, sound: sound1},
-        { x: 270, y: 170, sound: sound2},
-        { x: 240, y: 170, sound: sound3},
-        { x: 570, y: 170, sound: sound4},
-        { x: 120, y: 290, sound: sound5},
-        { x: 270, y: 290, sound: sound6},
-        { x: 420, y: 290, sound: sound7},
-        { x: 570, y: 290, sound: sound8}
+        { x: 80, y: 170, sound: sound0},
+        { x: 230, y: 170, sound: sound1},
+        { x: 380, y: 170, sound: sound2},
+        { x: 530, y: 170, sound: sound3},
+        { x: 80, y: 330, sound: sound4},
+        { x: 230, y: 330, sound: sound5},
+        { x: 380, y: 330, sound: sound6},
+        { x: 530, y: 330, sound: sound7}
     ]
 }
 
 function setup() {
-    createCanvas(700, 330);
-    //charcoal blue color
-    background(62, 80, 91);
+    createCanvas(700, 430);
+    //deep space blue color
+    background(0, 48, 69);
 
-    slider = createSlider(0, 1, 0.5, 0.05);
-    slider.position(allSliders[0].x, allSliders[0].y);
-    slider.size(90);
+    //creating all of the sliders for volume control
+    slider0 = createSlider(0, 1, 0.5, 0.05);
+    slider0.position(allSliders[0].x, allSliders[0].y);
+    slider0.size(90);
+    //adding class of slider to each slider
+    slider0.class("slider");
+
+    slider1 = createSlider(0, 1, 0.5, 0.05);
+    slider1.position(allSliders[1].x, allSliders[1].y);
+    slider1.size(90);
+    slider1.class("slider");
+
+    slider2 = createSlider(0, 1, 0.5, 0.05);
+    slider2.position(allSliders[2].x, allSliders[2].y);
+    slider2.size(90);
+    slider2.class("slider");
+
+    slider3 = createSlider(0, 1, 0.5, 0.05);
+    slider3.position(allSliders[3].x, allSliders[3].y);
+    slider3.size(90);
+    slider3.class("slider");
+
+    slider4 = createSlider(0, 1, 0.5, 0.05);
+    slider4.position(allSliders[4].x, allSliders[4].y);
+    slider4.size(90);
+    slider4.class("slider");
+
+    slider5 = createSlider(0, 1, 0.5, 0.05);
+    slider5.position(allSliders[5].x, allSliders[5].y);
+    slider5.size(90);
+    slider5.class("slider");
+
+    slider6 = createSlider(0, 1, 0.5, 0.05);
+    slider6.position(allSliders[6].x, allSliders[6].y);
+    slider6.size(90);
+    slider6.class("slider");
+
+    slider7 = createSlider(0, 1, 0.5, 0.05);
+    slider7.position(allSliders[7].x, allSliders[7].y);
+    slider7.size(90);
+    slider7.class("slider");
 }
 
 
@@ -51,11 +97,13 @@ function draw() {
     for(let btn of myButtons) {
         let hover = mouseX > btn.x && mouseX < btn.x + btn.w && mouseY > btn.y && mouseY < btn.y + btn.h;
         if (hover) {
-            //soft apricot color
-            fill(255, 219, 181);
+            //turquoise color
+            fill(0, 236, 221);
+            strokeWeight(2);
+            stroke(0, 21, 36);
         } else {
-            //sage green color
-            fill(112, 174, 110);
+            //lemon lime color
+            fill(194, 232, 18);
             strokeWeight(2);
             stroke(0, 21, 36);
         }
@@ -67,7 +115,15 @@ function draw() {
         textAlign(CENTER, CENTER);
         text(btn.name, btn.x + btn.w / 2, btn.y + btn.h / 2);
     }
-    allSliders[0].sound.setVolume(slider.value());
+    //telling each slider which sound volume it will control
+    allSliders[0].sound.setVolume(slider0.value());
+    allSliders[1].sound.setVolume(slider1.value());
+    allSliders[2].sound.setVolume(slider2.value());
+    allSliders[3].sound.setVolume(slider3.value());
+    allSliders[4].sound.setVolume(slider4.value());
+    allSliders[5].sound.setVolume(slider5.value());
+    allSliders[6].sound.setVolume(slider6.value());
+    allSliders[7].sound.setVolume(slider7.value());
 }
 
 function mousePressed () {
@@ -76,8 +132,6 @@ function mousePressed () {
         if(mouseX > btn.x && mouseX < btn.x + btn.w && mouseY > btn.y && mouseY < btn.y + btn.h) {
             console.log('the button has been clicked', btn);
             btn.sound.play();
-        } //else {
-            //btn.sound.stop();
-        //}
+        }
     }
 }
